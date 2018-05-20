40143815b022da1d19490e79c8002eb43514350f
Craig Marshall <craig9@gmail.com>
2018-05-21 08:44:50 +1000

    Incorporate old content (in a temporary way)

---
+++ old/metalwork/index.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+    <title>Craig Marshall - Metalwork</title>
+    <link href='/css/style.css' rel='stylesheet' type='text/css'>
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+</head>
+
+<body>
+<div class="content">
+
+<h1>Metalwork</h1>
+
+<p>Some pics of the metal items I&apos;ve made</p>
+
+<a href="../img/metalwork/201412-bottle-opener.jpg"><img src="../img/metalwork/201412-bottle-opener.thumb.jpg" alt="Five minute Bottle Opener - December 2014" /></a>
+
+<a href="../img/metalwork/201311-welding-table-before.jpg"><img src="../img/metalwork/201311-welding-table-before.thumb.jpg" alt="Welding Table Before - November 2013" /></a>
+
+<a href="../img/metalwork/201311-welding-table-after.jpg"><img src="../img/metalwork/201311-welding-table-after.thumb.jpg" alt="Welding Table After - November 2013" /></a>
+
+<a href="../img/metalwork/201311-motor-mount.jpg"><img src="../img/metalwork/201311-motor-mount.thumb.jpg" alt="Fabricated Motor Mount - November 2013" /></a>
+
+<a href="../img/metalwork/201310-welding-cart.jpg"><img src="../img/metalwork/201310-welding-cart.thumb.jpg" alt="Welding Cart - October 2013" /></a>
+
+<a href="../img/metalwork/201310-vise-before.jpg"><img src="../img/metalwork/201310-vise-before.thumb.jpg" alt="Dawn 4 1/2 SP Vise Refinish - October 2013" /></a>
+
+<a href="../img/metalwork/201310-vise-after.jpg"><img src="../img/metalwork/201310-vise-after.thumb.jpg" alt="Dawn 4 1/2 SP Vise Refinish - October 2013" /></a>
+
+<a href="../img/metalwork/201207-smoker2.jpg"><img src="../img/metalwork/201207-smoker2.thumb.jpg" alt="Cold Smoker Cabinet - July 2012" /></a>
+
+<a href="../img/metalwork/201207-smoker.jpg"><img src="../img/metalwork/201207-smoker.thumb.jpg" alt="Cold Smoker Cabinet - July 2012" /></a>
+
+<a href="../img/metalwork/201205-snapped-vise1.jpg"><img src="../img/metalwork/201205-snapped-vise1.thumb.jpg" alt="Snapped Vise Repair - May 2012" /></a>
+
+<a href="../img/metalwork/201205-snapped-vise2.jpg"><img src="../img/metalwork/201205-snapped-vise2.thumb.jpg" alt="Snapped Vise Repair - May 2012" /></a>
+
+<a href="../img/metalwork/201111-timber-rack.jpg"><img src="../img/metalwork/201111-timber-rack.thumb.jpg" alt="Timber Rack - Built with Jason Groombridge - November 2011" /></a>
+
+<a href="../img/metalwork/201111-timber-rack2.jpg"><img src="../img/metalwork/201111-timber-rack2.thumb.jpg" alt="Timber Rack - Built with Jason Groombridge - November 2011" /></a>
+
+<a href="../img/metalwork/201111-roof-frame2.jpg"><img src="../img/metalwork/201111-roof-frame2.thumb.jpg" alt="Porch Roof Frame 2 - November 2011" /></a>
+
+<a href="../img/metalwork/201111-roof-frame1.jpg"><img src="../img/metalwork/201111-roof-frame1.thumb.jpg" alt="Porch Roof Frame - November 2011" /></a>
+
+<a href="../img/metalwork/201111-greenhouse.jpg"><img src="../img/metalwork/201111-greenhouse.thumb.jpg" alt="Steel Greenhouse - Built with Jason Groombridge - November 2011" /></a>
+
+<a href="../img/metalwork/200912-woodburning-stove.jpg"><img src="../img/metalwork/200912-woodburning-stove.thumb.jpg" alt="Woodburning Stove - December 2009" /></a>
+
+</div>
+
+<div class="footer">
+    &copy; 2015 <a href="mailto:craig9@gmail.com">Craig Marshall</a>
+</div>
+
+<script>
+    $( "a > img" ).each(function(){
+        $(this).after('<br/>' + $(this).attr("alt"));
+        $(this).parent().css("display", "inline-block");
+    });
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89181730-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ old/woodwork/index.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+    <title>Craig Marshall - Woodwork</title>
+    <link href='/css/style.css' rel='stylesheet' type='text/css'>
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+</head>
+
+<body>
+<div class="content">
+
+<h1>Woodwork</h1>
+
+<p>In 2007, I left my job as an IT Support Engineer, and became an apprentice furniture maker (informally) with a very generous and talented furniture maker in the UK called Jonathan Shaw. I spent 3 years with him and learned an enormous amount, and will forever be in debt to him for that.</p>
+
+<p>Not much has happened since 2011, when I moved to Tasmania. I am trying to gradually build up usable workareas and collections of tools for woodworking, metalworking and electronics tinkering.</p>
+
+<p>Here are some of the better pictures I took, in reverse chronological order. Click any pic to see a larger version.</p>
+
+<a class="sign" href="../img/woodwork/201412-clamp-light.jpg"><img src="../img/woodwork/201412-clamp-light.thumb.jpg" alt="Clamp Lights - December 2014" /></a>
+
+<a class="sign" href="../img/woodwork/201411-woodgears-shelf.jpg"><img src="../img/woodwork/201411-woodgears-shelf.thumb.jpg" alt="Woodgears Shelf - November 2014" /></a>
+
+<a class="sign" href="../img/woodwork/201411-timber-rack.jpg"><img src="../img/woodwork/201411-timber-rack.thumb.jpg" alt="Timber Rack - November 2014" /></a>
+
+<a class="sign" href="../img/woodwork/201404-tablesaw-accessories.jpg"><img src="../img/woodwork/201404-tablesaw-accessories.thumb.jpg" alt="Tablesaw Accessories - April 2014" /></a>
+
+<a class="sign" href="../img/woodwork/201403-mitresaw-fence.jpg"><img src="../img/woodwork/201403-mitresaw-fence.thumb.jpg" alt="Mitresaw Fence - March 2014" /></a>
+
+<a class="sign" href="../img/woodwork/201207-compressor-shed.jpg"><img src="../img/woodwork/201207-compressor-shed.thumb.jpg" alt="Compressor Shed - July 2012" /></a>
+
+<a class="sign" href="../img/woodwork/201207-chisel-rack.jpg"><img src="../img/woodwork/201207-chisel-rack.thumb.jpg" alt="Chisel Rack - July 2012" /></a>
+
+<a class="sign" href="../img/woodwork/201207-hammer-handle.jpg"><img src="../img/woodwork/201207-hammer-handle.thumb.jpg" alt="Tas Oak Hammer Handle - July 2012" /></a>
+
+<a class="sign" href="../img/woodwork/201108-planters.jpg"><img src="../img/woodwork/201108-planters.thumb.jpg" alt="Garden Planters - August 2011" /></a>
+
+<a class="sign" href="../img/woodwork/201105-guitar-jack.jpg"><img src="../img/woodwork/201105-guitar-jack.thumb.jpg" alt="Guitar Jack Repair - May 2011" /></a>
+
+<a class="sign" href="../img/woodwork/201001-oak-refectory.jpg"><img src="../img/woodwork/201001-oak-refectory.thumb.jpg" alt="Oak Refectory Table - January 2010" /></a>
+
+<a class="sign" href="../img/woodwork/200912-ash-sideboard.jpg"><img src="../img/woodwork/200912-ash-sideboard.thumb.jpg" alt="Ash Sideboard - December 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200906-niddy-noddy.jpg"><img src="../img/woodwork/200906-niddy-noddy.thumb.jpg" alt="Niddy Noddy - June 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200905-walnut-dining.jpg"><img src="../img/woodwork/200905-walnut-dining.thumb.jpg" alt="Walnut Dining Table - May 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200905-ash-daybed.jpg"><img src="../img/woodwork/200905-ash-daybed.thumb.jpg" alt="Ash Day Bed - May 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200904-mahogany-bedside.jpg"><img src="../img/woodwork/200904-mahogany-bedside.thumb.jpg" alt="Mahogany Bedside Table - April 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200904-elm-coatrack.jpg"><img src="../img/woodwork/200904-elm-coatrack.thumb.jpg" alt="Elm Coatrack - April 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200904-oak-table-rebuild.jpg"><img src="../img/woodwork/200904-oak-table-rebuild.thumb.jpg" alt="Oak Table Rebuild - April 2009" /></a>
+
+<a class="sign" href="../img/woodwork/200811-basketmaking.jpg"><img src="../img/woodwork/200811-basketmaking.thumb.jpg" alt="Basketmaking - November 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200808-oak-divider.jpg"><img src="../img/woodwork/200808-oak-divider.thumb.jpg" alt="Oak Room Divider (Collaboration) - July 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200807-dovetailed-walnut-box.jpg"><img src="../img/woodwork/200807-dovetailed-walnut-box.thumb.jpg" alt="Dovetailed Walnut Box - July 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200806-oak-coffee-table.jpg"><img src="../img/woodwork/200806-oak-coffee-table.thumb.jpg" alt="Oak Coffee Table - June 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200806-electric-guitar.jpg"><img src="../img/woodwork/200806-electric-guitar.thumb.jpg" alt="Custom Electric Guitar - June 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200806-oak-door.jpg"><img src="../img/woodwork/200806-oak-door.thumb.jpg" alt="Basic Oak door - June 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200806-glazed-doors.jpg"><img src="../img/woodwork/200806-glazed-doors.thumb.jpg" alt="Glazed doors - June 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200805-iroko-gate.jpg"><img src="../img/woodwork/200805-iroko-gate.thumb.jpg" alt="Iroko Gate - May 2008" /></a>
+
+i<a class="sign" href="../img/woodwork/200805-brown-oak-bedside.jpg"><img src="../img/woodwork/200805-brown-oak-bedside.thumb.jpg" alt="Brown Oak Bedside Table - May 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200804-domino-alternative.jpg"><img src="../img/woodwork/200804-domino-alternative.thumb.jpg" alt="Festool Domino Alternative (experiment) - April 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200803-barn-window.jpg"><img src="../img/woodwork/200803-barn-window.thumb.jpg" alt="Barn Window (Collaboration) - March 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200803-oak-workbench.jpg"><img src="../img/woodwork/200803-oak-workbench.thumb.jpg" alt="Solid Oak Workbench - March 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200803-walnut-doors.jpg"><img src="../img/woodwork/200803-walnut-doors.thumb.jpg" alt="Walnut Doors - March 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200802-mahogany-vanity.jpg"><img src="../img/woodwork/200802-mahogany-vanity.thumb.jpg" alt="Mahogany Vanity Table - February 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200802-mahogany-vanity-cad.png"><img src="../img/woodwork/200802-mahogany-vanity-cad.thumb.png" alt="Mahogany Vanity Table CAD Drawing - February 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200802-mahogany-table-after.jpg"><img src="../img/woodwork/200802-mahogany-table-after.thumb.jpg" alt="Mahogany Table Rebuild After - February 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200802-mahogany-table-before.jpg"><img src="../img/woodwork/200802-mahogany-table-before.thumb.jpg" alt="Mahogany Table Rebuild Before - February 2008" /></a>
+
+<a class="sign collaboration" href="../img/woodwork/200802-oak-bookshelf.jpg"><img src="../img/woodwork/200802-oak-bookshelf.thumb.jpg" alt="Oak Bookshelf (Collaboration) - February 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200802-oak-bookshelf-cad.png"><img src="../img/woodwork/200802-oak-bookshelf-cad.thumb.png" alt="Oak Bookshelf CAD Drawing - February 2008" /></a>
+
+<a class="sign collaboration" href="../img/woodwork/200802-mahogany-doors.jpg"><img src="../img/woodwork/200802-mahogany-doors.thumb.jpg" alt="Mahogany Doors (Collaboration) - February 2008" /></a>
+
+<a class="sign" href="../img/woodwork/200801-oak-tv-cabinet.jpg"><img src="../img/woodwork/200801-oak-tv-cabinet.thumb.jpg" alt="Oak TV Cabinet - January 2008" /></a>
+
+<a class="sign collaboration" href="../img/woodwork/200801-builtin-bookcase.jpg"><img src="../img/woodwork/200801-builtin-bookcase.thumb.jpg" alt="Built-in Bookcase (collaboration) - January 2008" /></a>
+
+</div>
+
+<div class="footer">
+    &copy; 2015 <a href="mailto:craig9@gmail.com">Craig Marshall</a>
+</div>
+
+<script>
+    $( "a > img" ).each(function(){
+        $(this).after('<br/>' + $(this).attr("alt"));
+        $(this).parent().css("display", "inline-block");
+    });
+
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89181730-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
+</body>
+</html>
